544412d4c696c17f05ee9956e1497ea38734254a
Sam Green <sam.green7777@yahoo.com>
2024-11-13 13:35:00 +0000

    remover extra line in the setting

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -104,7 +104,6 @@ INSTALLED_APPS = [
     'allauth.account', 
     'allauth.socialaccount', 
     'dj_rest_auth.registration',
-    'dj_rest_auth.registration',
     'corsheaders',
 
     'profiles',
